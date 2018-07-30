fdebb6a952ab59b62472328a216e2f3a3cce88a2
TheEmeraldRanger <TheEmeraldRanger@users.noreply.github.com>
2018-07-30 14:32:58 -0400

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,3 +1,35 @@
+var yn = document.getElementbyId("yn")
+var d6 = document.getElementbyId("d6")
+var d20 = document.getElementbyId("d20")
+var d100 = document.getElementbyId("d100")
 
+var rtc =  result.textcontent
 
+yn.onclick = function() {
+ var selectrando = Math.floor(Math.random()*2)+1 
+ switch(selectrando){
+   case 1:
+     rtc = "yes";
+     brea = "
+   case 2:
+     rtc = "no";
+     break
+ }
+}
 
+d6.onclick = function() {
+  var selectrando = Math.floor(Math.random()*6)+1
+  var randnum2 = Math.floor(Math.random()*4)+1
+  switch(selectrando){
+    case 1:
+      switch(randnum2)
+        case 1:
+          rtc = "Take a wildddddddddddd guess......... [You rolled a ONE, Good luck (^_-)]"
+          break
+        case 2:
+          rtc = "R.I.P. That_Guy [Rolled a ONE]"
+          break
+        case 3:
+          rtc = ""
+  }
+}
